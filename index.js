a57c23599a5ada867d9e02e5e077beb9faf00a27
bossteve1 <bossteve98@gmail.com>
2024-07-12 14:53:44 +0300

    Add async checkweather function

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const apiKey = 'ed073be48ab9f72b4a7aad430b248120';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=nairobi'
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    const data = await response.json();
+
+    console.log(data)
+}
+checkWeather()
